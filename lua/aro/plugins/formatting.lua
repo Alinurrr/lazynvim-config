503d7f6363be46d4428d9f5cc21a2bce4626f8d2
Ali N <alinoer.rachman@gmail.com>
2025-08-26 14:41:48 +0700

    disabled format on save

--- lua/aro/plugins/formatting.lua
+++ lua/aro/plugins/formatting.lua
@@ -22,11 +22,11 @@ return {
         python = { "isort", "black" },
       },
 
-      format_on_save = {
-        lsp_fallback = false,
-        async = false,
-        timeout_ms = 3000,
-      },
+      -- format_on_save = {
+      --   lsp_fallback = false,
+      --   async = false,
+      --   timeout_ms = 3000,
+      -- },
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
