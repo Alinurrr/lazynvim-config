c9728a796af084ed8c7d7cd18749ef4a95bb4a50
Ali N <alinoer.rachman@gmail.com>
2025-08-26 09:48:21 +0700

    remove pyton

--- lua/aro/plugins/lsp/mason.lua
+++ lua/aro/plugins/lsp/mason.lua
@@ -36,7 +36,6 @@ return {
         "graphql",
         "emmet_ls",
         "prismals",
-        "pyright",
         "eslint",
       },
     })
@@ -45,9 +44,6 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
         "eslint_d",
       },
     })
